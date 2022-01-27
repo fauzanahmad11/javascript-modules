c0e81cd49ffee433b8cb408bf0436630a3b0dd90
Fauzan Ahmad <fauzanahmadgojeck@gmail.com>
2022-01-27 19:01:05 +0700

    with module

---
+++ library.js
@@ -0,0 +1,14 @@
+/*
+## Tanpa Module
+● Sebelum kita membahas JavaScript Module, sekarang kita akan coba dulu tanpa menggunakan module
+● Saat kita membuat file JavaScript, lalu kita load file JavaScript tersebut di web, secara default semua kode JavaScript di file tersebut bisa diakses, baik itu variable, function atau class
+*/
+
+// Kode : JavaScript Library
+function sayHello(name) {
+    console.log(`Hello ${name}`);
+}
+
+function sayGoodBye(name) {
+    console.log(`Good Bye ${name}`);
+}
---
+++ say.js
@@ -0,0 +1,3 @@
+// Kode : JavaScript Module
+const sayHello = name => console.log(`Hello ${name}`);
+const sayGoodBye = name => console.log(`Good Bye ${name}`);
